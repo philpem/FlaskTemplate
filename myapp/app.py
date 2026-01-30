3bdda610ec6cf51c94cfb20b4a561cb9c922a373
Philip Pemberton <philpem@philpem.me.uk>
2026-01-30 02:39:41 +0000

    app: reduce use of global app

--- myapp/app.py
+++ myapp/app.py
@@ -70,6 +70,34 @@ def create_app(config_name=None):
 
 		sqltap_sess = sqltap.start(user_context_fn = context_fn)
 
+	# -- main menu handing (context processor) --
+	@app.context_processor
+	def inject_menu():
+		"""
+		Context processor for the main menu
+
+		Adds the main menu data into the template context. Menu items are sorted by sort-order, then (case-insensitively) by label.
+		"""
+		return dict(menu=sorted(app._myapp_menudata,
+								key=lambda mi: (mi['sortorder'], mi['label'].lower())))
+
+
+	# -- post-request teardown --
+	@app.teardown_request
+	def shutdown_session(exception=None):
+		# if database profiling is enabled, save a report
+		if app.config.get('DEBUG_DB_PROFILING', False) and not request.path.startswith('/static'):
+			# filter out any statistics which aren't for this request
+			stats_all = sqltap_sess.collect()
+			stats_req = list(filter(lambda x: x.user_context == g.req_id, sqltap_sess.collect()))
+			sqltap.report(stats_all, os.path.join(os.path.dirname(os.path.realpath(__file__)), "static/db_profile_report_all.html"))
+			sqltap.report(stats_req, os.path.join(os.path.dirname(os.path.realpath(__file__)), "static/db_profile_report_req.html"))
+		# close the database session
+		db.session.remove()
+
+	# Register login handlers
+	_register_login_handlers(app)
+
 	# Register blueprints
 	_register_blueprints(app)
 
@@ -98,6 +126,65 @@ def _register_blueprints(app):
 			app.logger.error(f"Failed to load blueprint {modname}: {e}")
 			continue
 
+def _register_login_handlers(app):
+    # -- login management --
+
+    from .database import User
+
+    # TODO -- For 'fresh_login_required' to work, we need a "reauthenticate" handler.
+    #   See https://github.com/maxcountryman/flask-login/blob/master/example/login-example.py for a code example
+    #login_manager.refresh_view = 'reauth'
+    #login_manager.needs_refresh_message = u'To protect your account, please re-authenticate to access this page.'
+
+    @login_manager.user_loader
+    def load_user(userid):
+        userrec = None
+        try:
+            userrec = User.query.filter(User.id == int(userid)).one()
+        except MultipleResultsFound:
+            app.logger.error("USER LOGIN FAILURE: User '%s' has a doppelganger (duplicate username found)")
+        except NoResultFound:
+            app.logger.warning("Userloader: id '%d' returned no results" % int(userid))
+            pass # with userrec = None
+        return userrec
+
+    @app.route("/login", methods=["GET","POST"])
+    def login():
+        class LoginForm(FlaskForm):
+            username=StringField("Username", validators=[DataRequired()])
+            password=PasswordField("Password", validators=[DataRequired()])
+            submit=SubmitField("Log in")
+
+        form = LoginForm()
+        if form.validate_on_submit():
+            # login and validate the user
+            userrec = None
+            try:
+                userrec = User.query.filter(User.username == form.username.data).one()
+            except MultipleResultsFound:
+                app.logger.error("USER LOGIN FAILURE: User '%s' has a doppelganger (duplicate username found)")
+            except NoResultFound:
+                pass # with userrec = None
+
+            if userrec is not None:
+                # check password
+                if userrec.checkPassword(form.password.data):
+                    login_user(userrec)
+                    #flash("Logged in successfully", "success")
+                    return redirect(request.args.get("next") or url_for("myapp_blueprints_index.index"))
+
+        if request.method == 'POST':
+            flash("Error logging in - please check your username and password and ensure that CAPS LOCK is turned off.", "error")
+        return render_template("login.html", form=form)
+
+    @app.route("/logout")
+    @login_required
+    def logout():
+        logout_user()
+        flash("You have now been logged out.", "info")
+        return redirect(url_for("login"))
+
+
 
 # create the application
 app = create_app()
@@ -106,88 +193,6 @@ application = app
 
 
 
-# -- main menu handing (context processor) --
-@app.context_processor
-def _myapp_contextproc_menu():
-	"""
-	Context processor for the main menu
-
-	Adds the main menu data into the template context. Menu items are sorted by sort-order, then (case-insensitively) by label.
-	"""
-	return dict(menu=sorted(app._myapp_menudata, key=lambda mi: (mi['sortorder'], mi['label'].lower())))
-
-
-# -- post-request teardown --
-@app.teardown_request
-def shutdown_session(exception=None):
-	# if database profiling is enabled, save a report
-	if app.config.get('DEBUG_DB_PROFILING', False) and not request.path.startswith('/static'):
-		# filter out any statistics which aren't for this request
-		stats_all = sqltap_sess.collect()
-		stats_req = list(filter(lambda x: x.user_context == g.req_id, sqltap_sess.collect()))
-		sqltap.report(stats_all, os.path.join(os.path.dirname(os.path.realpath(__file__)), "static/db_profile_report_all.html"))
-		sqltap.report(stats_req, os.path.join(os.path.dirname(os.path.realpath(__file__)), "static/db_profile_report_req.html"))
-	# close the database session
-	db.session.remove()
-
-# -- login management --
-
-from .database import User
-
-# TODO -- For 'fresh_login_required' to work, we need a "reauthenticate" handler.
-#   See https://github.com/maxcountryman/flask-login/blob/master/example/login-example.py for a code example
-#login_manager.refresh_view = 'reauth'
-#login_manager.needs_refresh_message = u'To protect your account, please re-authenticate to access this page.'
-
-@login_manager.user_loader
-def load_user(userid):
-	userrec = None
-	try:
-		userrec = User.query.filter(User.id == int(userid)).one()
-	except MultipleResultsFound:
-		app.logger.error("USER LOGIN FAILURE: User '%s' has a doppelganger (duplicate username found)")
-	except NoResultFound:
-		app.logger.warning("Userloader: id '%d' returned no results" % int(userid))
-		pass # with userrec = None
-	return userrec
-
-@app.route("/login", methods=["GET","POST"])
-def login():
-	class LoginForm(FlaskForm):
-		username=StringField("Username", validators=[DataRequired()])
-		password=PasswordField("Password", validators=[DataRequired()])
-		submit=SubmitField("Log in")
-
-	form = LoginForm()
-	if form.validate_on_submit():
-		# login and validate the user
-		userrec = None
-		try:
-			userrec = User.query.filter(User.username == form.username.data).one()
-		except MultipleResultsFound:
-			app.logger.error("USER LOGIN FAILURE: User '%s' has a doppelganger (duplicate username found)")
-		except NoResultFound:
-			pass # with userrec = None
-
-		if userrec is not None:
-			# check password
-			if userrec.checkPassword(form.password.data):
-				login_user(userrec)
-				#flash("Logged in successfully", "success")
-				return redirect(request.args.get("next") or url_for("myapp_blueprints_index.index"))
-
-	if request.method == 'POST':
-		flash("Error logging in - please check your username and password and ensure that CAPS LOCK is turned off.", "error")
-	return render_template("login.html", form=form)
-
-@app.route("/logout")
-@login_required
-def logout():
-	logout_user()
-	flash("You have now been logged out.", "info")
-	return redirect(url_for("login"))
-
-"""
 ###########
 # error page handling
 ###########
@@ -199,6 +204,5 @@ def not_found(e):
 @app.errorhandler(500)
 def internal_error(e):
     return render_template('errors/500.html'), 500
-"""
 
 # vim: ts=4 sw=4 noet
